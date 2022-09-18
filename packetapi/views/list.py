ed859f8ac145a9d3a7d26bc788b089bd0a6de0df
turner lynch <turnerjoylynch@gmail.com>
2022-09-17 20:41:33 -0700

    m@m list updates

--- packetapi/views/list.py
+++ packetapi/views/list.py
@@ -4,7 +4,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from packetapi.models.item import PacketItem
-
+from rest_framework.decorators import action
 from packetapi.models.list import PacketList
 from packetapi.models.user import PacketUser
 
@@ -32,6 +32,7 @@ class ListView(ViewSet):
         """
         data = request.data
         userId = PacketUser.objects.get(user=request.auth.user)
+        items = PacketItem.objects.get(pk=request.data["items"])
         
         list = PacketList.objects.create(
             userId = userId,
@@ -53,17 +54,14 @@ class ListView(ViewSet):
             Response -- Empty body with 204 status code
         """
         list = PacketList.objects.get(pk=pk)
-        list.list_name = request.data["list_name"]
-        
         item = PacketItem.objects.get(pk=pk)
-        # item.name = request.data["item_name"]
+        list.list_name = request.data["list_name"]
+
         
         lists = PacketList.objects.get(pk=request.data["lists"])
         item.lists = lists
-        
-        item.save()
+         
         list.save()
-
         return Response(None, status=status.HTTP_204_NO_CONTENT)
 
     def destroy(self, request, pk):
@@ -72,6 +70,40 @@ class ListView(ViewSet):
         return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 
+''' Actions to include the JOIN tables??
+'''
+
+@action(methods=['post'], detail=True)
+def join_list(self, request, pk):
+        """Post request to add a item to a list"""
+        list = PacketList.objects.get(pk=pk)
+        item = request.data['item']
+        list.items.add(item) 
+        return Response({'message': 'Item added'}, status=status.HTTP_201_CREATED)
+    
+@action(methods=['delete'], detail=True)
+def remove(self, request, pk):
+        """Delete request of a item to be removed from a list"""
+        item = PacketItem.objects.get(pk=pk)
+        list = PacketList.objects.get(pk=pk)
+        list.items.remove(item)
+        return Response({'message': 'Item removed'}, status=status.HTTP_204_NO_CONTENT)
+    
+    # @action(methods=['get'], detail=True)
+    # def partner_alert(self, request, pk):
+    #     """ Gets user's partner pairing """
+    #     try: 
+    #         group = Group.objects.get(pk=pk)
+    #         giver = Member.objects.get(user=request.auth.user)
+    #         partner = Partner.objects.get(giver_id=giver.id, group_id=group.id)
+    #         serializer = PartnerSerializer(partner)
+    #         return Response(serializer.data)
+    #     except Partner.DoesNotExist as ex:
+    #         return Response({'message': ex.arg[0]}, status=status.HTTP_404_NOT_FOUND)
+  
+    
+''' Actions to include the JOIN tables??
+'''
   
 class ListSerializer(serializers.ModelSerializer):
     """JSON serializer for lists """
